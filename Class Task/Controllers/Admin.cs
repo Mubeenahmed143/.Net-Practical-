0fb0e6ce0a3c84a0edbdb23991037398b99ff650
Mubeenahmed143 <ahmedsheikhmubeen395@gmail.com>
2024-11-23 16:48:40 +0500

    task complete ho gya frontend pr search ho rha hai or dashboard pr search work kr rha hai

---
+++ Class Task/Controllers/Admin.cs
@@ -0,0 +1,64 @@
+﻿using Class_Task.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Class_Task.Controllers
+{
+    public class Admin : Controller
+    {
+        PracticalContext db = new PracticalContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddPro()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPro(Product pg, IFormFile file)
+        {
+            var imageName = Path.GetFileName(file.FileName);
+            string imagePath = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/Image/");
+            string imagevalue = Path.Combine(imagePath, imageName);
+            using (var stream = new FileStream(imagevalue, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            var dbimage = Path.Combine("/Image/", imageName);
+            pg.Image = dbimage;
+            db.Products.Add(pg);
+            db.SaveChanges();
+
+            
+            return View();
+        }
+
+        public IActionResult ShowPro()
+        {
+            var showpro = db.Products.ToList();
+            return View(showpro);
+
+           
+        }
+
+        [HttpPost]
+        public IActionResult Search(string xyz)
+        {
+            var resultt = db.Products.Where(x => x.Name.Contains(xyz));
+            return View("ShowPro", resultt.ToList());
+        }
+        [HttpGet]
+        public IActionResult DeletePro(Product pg)
+        {
+
+            db.Products.Remove(pg);
+            db.SaveChanges();
+            return RedirectToAction("ShowPro");
+
+        }
+
+    }
+}
---
+++ Class Task/Controllers/User.cs
@@ -0,0 +1,31 @@
+﻿using Class_Task.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class_Task.Controllers
+{
+    public class User : Controller
+    {
+
+        PracticalContext db = new PracticalContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            var showpro = db.Products.ToList();
+            return View(showpro);
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Search(string xyz)
+        {
+            var resultt = db.Products.Where(x => x.Name.Contains(xyz));
+            return View("Shop", resultt.ToList());
+        }
+
+    }
+}
